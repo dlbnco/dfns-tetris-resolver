93edbbb037baebc9ed38368c4223292b3796b99e
Mateus Dal Bianco <mateus@dlbn.co>
2025-10-27 16:46:49 +0100

    chore: set up script

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,21 @@
-import { isColliding, Position } from "./lib/collision";
-import { parseGameString } from "./lib/game";
-import {
-  clearRowsIfFilled,
-  createEmptyGrid,
-  drawGrid,
-  fillGridWithPiecePosition,
-} from "./lib/grid";
+import * as readline from "readline";
+import { processGame } from "./lib/game";
+import { DEFAULT_GRID_HEIGHT, DEFAULT_GRID_WIDTH } from "./lib/grid";
 
-const DEFAULT_GRID_WIDTH = 10;
-const DEFAULT_GRID_HEIGHT = 100;
+const main = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
 
-const main = (
-  game: string,
-  width = DEFAULT_GRID_WIDTH,
-  height = DEFAULT_GRID_HEIGHT,
-) => {
-  let grid = createEmptyGrid(width, height);
-  const parsedGame = parseGameString(game);
-
-  for (const round of parsedGame) {
-    const piece = round[0];
-    const initialX = round[1];
-    const position: Position = [initialX, 0];
-    for (let y = 0; y < height; y++) {
-      const [x, y] = position;
-      const newY = y + 1;
-      if (isColliding(piece, [x, newY], grid)) {
-        continue;
-      }
-      position[1] = newY;
-    }
-    grid = fillGridWithPiecePosition(piece, position, grid);
-    grid = clearRowsIfFilled(grid);
+  for await (const line of rl) {
+    const height = processGame(line, DEFAULT_GRID_WIDTH, DEFAULT_GRID_HEIGHT);
+    console.log(height);
   }
-  grid = clearRowsIfFilled(grid);
-  //console.log("Last row:", grid[height - 1]);
-  //console.log(drawGrid(grid));
-  const tallestIndex = grid.findIndex((row) => row.some((cell) => cell === 1));
-  if (tallestIndex === -1) return 0;
-  return height - tallestIndex;
 };
 
-console.log("Run 1", main("O0,O2,O4,O6,O8"));
-console.log("Run 2", main("L0,J2,O4,L6,J8"));
-console.log("Run 3", main("T0,Z3,T5,J8"));
-console.log("Run 4", main("T0,Z3,T5,O2,O8"));
+main().catch((error) => {
+  console.error("Error:", error);
+  process.exit(1);
+});
